b0b40bef96c84a44c543abbc98bfe7e9c7ce3b85
arcticcloudfox <27johnson279@gmail.com>
2024-08-22 12:46:08 -0500

    src

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -3,13 +3,19 @@ import './App.css';
 import MovieList from './components/MovieList.jsx';
 import ChoresList from './components/ChoresList.jsx';
 import BookList from './components/BookList.jsx';
+import HobbyLinks from './components/hobbyLinks.jsx';
 
 function App() {
   return (
     <div className="App">
-      <MovieList />
       <ChoresList />
+      <div className = "similarComponents">
+      <MovieList />
+      <div style = {{border: '1px solid black'}}>
       <BookList />
+      </div>
+      <HobbyLinks />
+      </div>
     </div>
   );
 }
